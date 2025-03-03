dc453423e0b2508d827b2c9e360dc6100f391b99
cavanahssx <78207528+cavanahssx@users.noreply.github.com>
2025-03-03 09:36:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 // URL OAuth2
+const DISCORD_OAUTH2_URL = `https://discord.com/api/oauth2/authorize?client_id=${process.env.CLIENT_ID}&redirect_uri=${encodeURIComponent(process.env.REDIRECT_URI)}&response_type=code&scope=identify%20guilds.members.read`;
+
+// Redirection vers Discord pour l'authentification
 const { addUserToSheet } = require('./googleSheets');
 
 app.get('/auth/discord/callback', async (req, res) => {
